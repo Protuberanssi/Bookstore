1f4fa3bf4b2a80767719c065d6da76407774c134
Tatu Korpela <tatu.korpela@netti.fi>
2017-09-12 09:48:21 +0300

    First commit

---
+++ src/main/java/com/example/Bookstore/models/Book.java
@@ -0,0 +1,76 @@
+package com.example.Bookstore.models;
+
+public class Book{
+	private Long id;
+	private String title;
+	private String author;
+	private Integer year;
+	private String isbn;
+	private Integer price;
+	
+	public Book(Long id, String title, String author, Integer year, String isbn, Integer price) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "]";
+	}
+	
+	
+}
