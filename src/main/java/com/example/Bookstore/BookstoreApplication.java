fb4cf667bf8496a5040aa3e362b6eff0c5d5d659
Tatu Korpela <tatu.korpela@netti.fi>
2017-12-11 21:44:49 +0200

    Lisäyksiä

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.Bookstore.models.Book;
 import com.example.Bookstore.models.BookRepository;
+import com.example.Bookstore.models.User;
+import com.example.Bookstore.models.UserRepository;;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -20,14 +22,19 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository){
+	public CommandLineRunner bookDemo(BookRepository brepository, UserRepository urepository){
 		return (args) ->{
 			log.info("save a couple of books");
-			repository.save(new Book(null, "A Farewell to Arms", "Ernest Hemingway", 1929, "1232323-21", 20.00));
-			repository.save(new Book(null, "Animal Farm", "George Orwell", 1945, "2212345-5", 20.00));
+			brepository.save(new Book(null, "A Farewell to Arms", "Ernest Hemingway", 1929, "1232323-21", 20.00));
+			brepository.save(new Book(null, "Animal Farm", "George Orwell", 1945, "2212345-5", 29.99));
+
+			User user1 = new User("user", "$2a$10$TBGA50vNgT6elbZuHw3pC.uDD90SlPiMnlvAb/hj6XxDqjOfawfP.", "USER");
+			User user2 = new User("admin", "$2a$10$68sgSp8a6ofHdZwffk9/9uNnbiIcrQYFz/ssfIBlTglt/HLgA9Pwi", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 			
 			log.info("fetch all books");
-			for (Book book : repository.findAll()){
+			for (Book book : brepository.findAll()){
 				log.info(book.toString());
 			}
 		};
--- src/main/java/com/example/Bookstore/controller/BookController.java
+++ src/main/java/com/example/Bookstore/controller/BookController.java
@@ -1,21 +1,31 @@
 package com.example.Bookstore.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.Bookstore.models.Book;
 import com.example.Bookstore.models.BookRepository;
 
 @Controller
 public class BookController{
+	
 	@Autowired
 	private BookRepository repository;
 	
-	@RequestMapping(value="/index")
+	@RequestMapping(value="/login")
+	public String login(){
+		return "login";
+	}
+	
+	@RequestMapping(value="/booklist") // index
 		public String doSomething(Model model){
 		model.addAttribute("book", new Book());
 		model.addAttribute("books", repository.findAll());
@@ -27,6 +37,19 @@ public class BookController{
 		return "booklist";
 	}
 	
+	//RESTful service
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) repository.findAll();
+	}
+	
+	//RESTful service to get book by id
+	@RequestMapping(value="/book/{id}", method=RequestMethod.GET)
+	public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId){
+		return repository.findOne(bookId);
+	}
+	
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping(value="/add")
 	public String addBook(Model model){
 		model.addAttribute("book", new Book());
