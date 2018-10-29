794e612279ead836120dec1fba8bd7f9a1da38f5
Tatu Korpela <tatusamulikorpela@gmail.com>
2018-10-29 19:14:59 +0200

    Mapping

--- src/main/java/com/example/Bookstore/controller/BookController.java
+++ src/main/java/com/example/Bookstore/controller/BookController.java
@@ -25,7 +25,7 @@ public class BookController{
 		return "login";
 	}
 	
-	@RequestMapping(value="/booklist") // index
+	@RequestMapping(value="/") // index
 		public String doSomething(Model model){
 		model.addAttribute("book", new Book());
 		model.addAttribute("books", repository.findAll());
